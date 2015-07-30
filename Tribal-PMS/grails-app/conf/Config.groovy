3be86c7fe2d458abab9f6d04559aeaf35b4ab5f0
Ferenc Szekely <ferenc.szekely@voicemailtel.com>
2015-07-30 18:07:38 +0300

    changed path for jquery ui icons

--- Tribal-PMS/grails-app/conf/Config.groovy
+++ Tribal-PMS/grails-app/conf/Config.groovy
@@ -142,6 +142,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/**/js/**':        ['permitAll'],
 	'/**/css/**':       ['permitAll'],
 	'/**/images/**':    ['permitAll'],
+'/assets/images/**':    ['permitAll'],
 	'/**/favicon.ico':  ['permitAll']
 ]
 
